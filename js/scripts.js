0ef312c231623481af3f4ba485af611996157d58
Trevor Simonson <simonsot@gmatc.matc.edu>
2023-12-02 16:47:20 -0600

    fixed duplicate button events

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ init();
 
 async function init(){
     document.getElementById("current-date").appendChild(document.createTextNode(date));
-    //var neos = await fetch(apiLink, {mode: 'no-cors'})
     getNeoAPI(apiLink);
 }
 
@@ -30,25 +29,24 @@ function createNeosElements(neos){
     var astList = document.getElementById("asteroid-list");
 
     var asts = neos[date];
-    console.log(asts);
 
+    console.log(asts);
     for(var i = 0; i < asts.length; i++){
         
         var ast = asts[i];
-        console.log(ast);
         var div = document.createElement('div');
         div.classList.add("asteroid-info")
 
         var astName = document.createElement('h2');
         var astSize = document.createElement('p');
         var astHazard = document.createElement('p');
-        console.log(ast.name);
         astName.appendChild(document.createTextNode("Asteroid Name: " + ast.name));
         astSize.appendChild(document.createTextNode("Estimated Size: " + 
                 ast.estimated_diameter.meters.estimated_diameter_min.toFixed(2) + " - "+ 
                 ast.estimated_diameter.meters.estimated_diameter_max.toFixed(2) + " meters"));
         astHazard.appendChild(document.createTextNode("Potentially Hazardous: " +ast.is_potentially_hazardous_asteroid));
 
+        
         if(ast.is_potentially_hazardous_asteroid){
             div.style.backgroundColor = "#FF7276";
         }
@@ -68,49 +66,33 @@ function linkButtons(links){
         var nextButton = document.getElementById('next-button');
         var previousButton = document.getElementById('previous-button');
 
-        nextButton.addEventListener('click', function(){
-            var list = document.getElementById('asteroid-list')
-            list.innerHTML = "";
-            var loadingRing = addLoadingRing();
-            date = getDateFromLink(nextLink);
-            getNeoAPI(nextLink).then(e => {
-                removeLoadingRing(loadingRing);
-            });
-            setDate();
-           
-        });
+        wireButton(nextButton, nextLink);
+        wireButton(previousButton, previousLink);      
+}
 
-        previousButton.addEventListener('click', function(){
+function wireButton(button, link){
+        console.log(link);
+        
+        button.onclick = function(){
             var list = document.getElementById('asteroid-list')
             list.innerHTML = "";
-            var loadingRing = addLoadingRing();
-            date = getDateFromLink(previousLink);
-            getNeoAPI(previousLink).then(e => {
-                removeLoadingRing(loadingRing);
+            var loadingRing = addLoadingRing(list);
+            date = getDateFromLink(link);
+            getNeoAPI(link).then(e => {
+                loadingRing.remove();
             });
             setDate();
             
-        });
-        
-        
+        };
 }
 
-function addLoadingRing(){
-    var list = document.getElementById("asteroid-list");
+function addLoadingRing(list){
     var div = document.createElement('div');
     div.classList.add("lds-dual-ring")
     list.appendChild(div);
     return div;
 
 }
-
-function removeLoadingRing(div){
-    var list = document.getElementById("asteroid-list");
-    list.removeChild(div);
-
-}
-
-
 
 // ******* UTILITY **************
 function setDate(){
